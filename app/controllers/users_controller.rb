d419c683b80957d6dc424d9466c14e0f618beb17
mangochiman <mangochiman@gmail.com>
2016-10-15 13:48:26 +0200

    Working on authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,17 @@ class UsersController < ApplicationController
   def void_users
     
   end
+
+  def authenticate_user
+    user = User.find_by_username(params['username'])
+    logged_in_user = User.authenticate(params[:username], params[:password])
+
+    if logged_in_user
+      session[:user] = user
+      redirect_to("/pages") and return
+    else
+      flash[:error] = "Invalid username or password"
+      redirect_to("/login") and return
+    end
+  end
 end
