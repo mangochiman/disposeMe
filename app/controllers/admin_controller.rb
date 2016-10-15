b85510fabfb397e63063d6079872cdf99cdc68c7
mangochiman <mangochiman@gmail.com>
2016-10-15 15:59:05 +0200

    UI

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,10 @@
 class AdminController < ApplicationController
+  def create_new_drug
+    drug = Drug.new
+    drug.name = params[:drug_name]
+    drug.batch_no = params[:batch_no]
+    drug.expiry_date = params[:expiry_date].to_date rescue params[:expiry_date]
+    drug.save
+    redirect_to("/view_drug_menu") and return
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     
   end
+
+  def view_drug_menu
+    @drugs = Drug.find(:all, :order => "drug_id DESC")
+  end
 end
