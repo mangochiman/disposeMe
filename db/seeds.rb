08d76cfc7de6b77a47951bf987346f2f47818bf0
mangochiman <mangochiman@gmail.com>
2018-01-30 11:12:40 +0200

    Changing default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 user = User.new
-user.first_name = 'E&J'
-user.last_name = 'J&E'
-user.email = 'ej@gmail.com'
+user.first_name = 'Demo'
+user.last_name = 'Account'
+user.email = 'demo@gmail.com'
 user.phone_number = '0111111111'
-user.password = 'user'
-user. username = 'user'
+user.password = 'demo'
+user. username = 'demo'
 user.save
 
 puts "Username =  'user' \n Password = 'user'"
